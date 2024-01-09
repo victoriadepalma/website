061da463732b715c8f362bcd87cc06befe9ffb23
Victoria De Palma <victoriadepalma@Victorias-MacBook-Air.local>
2024-01-09 19:03:23 -0400

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <h1>Victoria De Palma</h1>
+  )
+}
+
+export default App
